a8c7d073c2572687476ee8e0de256bf0a74ac75a
Snowstorm08 <snowmandot0520@gmail.com>
2026-05-14 18:58:03 +0900

    Update TestBytesNative.sol

--- contracts/TestBytesNative.sol
+++ contracts/TestBytesNative.sol
@@ -1,49 +1,54 @@
-pragma solidity <0.9.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
 contract TestBytesNative {
-  struct Data {
-    bytes2 bytes2_field;
-    bytes10 bytes10_field;
-    bytes17 bytes17_field;
-    bytes31 bytes31_field;
+    struct Data {
+        bytes2 bytes2Field;
+        bytes10 bytes10Field;
+        bytes17 bytes17Field;
+        bytes31 bytes31Field;
+    }
 
-  }
-  mapping(address => Data) public contracts;
-  function getTestBytesBytes2(address key) public view returns (bytes2) {
-    return contracts[key].bytes2_field;
-  }
+    mapping(address => Data) public contractsData;
 
-  function getTestBytesBytes10(address key) public view returns (bytes10) {
-    return contracts[key].bytes10_field;
-  }
+    function getTestBytesBytes2(address key) external view returns (bytes2) {
+        return contractsData[key].bytes2Field;
+    }
 
-  function getTestBytesBytes17(address key) public view returns (bytes17) {
-    return contracts[key].bytes17_field;
-  }
+    function getTestBytesBytes10(address key) external view returns (bytes10) {
+        return contractsData[key].bytes10Field;
+    }
 
-  function getTestBytesBytes31(address key) public view returns (bytes31) {
-    return contracts[key].bytes31_field;
-  }
+    function getTestBytesBytes17(address key) external view returns (bytes17) {
+        return contractsData[key].bytes17Field;
+    }
 
-  function getBytes2FromBytes32(bytes32 value) public pure returns(bytes2) {
-    return bytes2(value);
-  }
+    function getTestBytesBytes31(address key) external view returns (bytes31) {
+        return contractsData[key].bytes31Field;
+    }
 
-  function getBytes10FromBytes32(bytes32 value) public pure returns(bytes10) {
-    return bytes10(value);
-  }
+    function getBytes2FromBytes32(bytes32 value) external pure returns (bytes2) {
+        return bytes2(value);
+    }
 
-  function getBytes17FromBytes32(bytes32 value) public pure returns(bytes17) {
-    return bytes17(value);
-  }
+    function getBytes10FromBytes32(bytes32 value) external pure returns (bytes10) {
+        return bytes10(value);
+    }
 
-  function getBytes31FromBytes32(bytes32 value) public pure returns(bytes31) {
-    return bytes31(value);
-  }
+    function getBytes17FromBytes32(bytes32 value) external pure returns (bytes17) {
+        return bytes17(value);
+    }
 
-  function storeTestBytes(address key, bytes32 value) public {
-    Data memory data = Data({bytes2_field: bytes2(value), bytes10_field: bytes10(value),
-      bytes17_field: bytes17(value), bytes31_field: bytes31(value)});
-    contracts[key] = data;
-  }
+    function getBytes31FromBytes32(bytes32 value) external pure returns (bytes31) {
+        return bytes31(value);
+    }
+
+    function storeTestBytes(address key, bytes32 value) external {
+        contractsData[key] = Data({
+            bytes2Field: bytes2(value),
+            bytes10Field: bytes10(value),
+            bytes17Field: bytes17(value),
+            bytes31Field: bytes31(value)
+        });
+    }
 }
